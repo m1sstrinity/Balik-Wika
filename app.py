6151e39b3ca1752680c2deefc9d97be139aa06a5
m1sstrinity <threeniteacamille@gmail.com>
2025-11-05 01:33:57 +0800

    removed credentials

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ app.secret_key = os.environ.get('SECRET_KEY', 'app_secret_123')
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'rosalestrinity0625@gmail.com'
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'lkaa rgsy ywtj tpjz')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 mail = Mail(app)
 
 # Registers
